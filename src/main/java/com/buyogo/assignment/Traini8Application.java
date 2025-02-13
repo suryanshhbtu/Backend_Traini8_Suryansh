59024a48c51f6631f5ee2404cd81717f6b4a3e64
Suryansh Srivastava <suryanshhbtu@gmail.com>
2025-02-13 21:06:03 +0530

    Initial commit

---
+++ src/main/java/com/buyogo/assignment/Traini8Application.java
@@ -0,0 +1,13 @@
+package com.buyogo.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Traini8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Traini8Application.class, args);
+	}
+
+}
